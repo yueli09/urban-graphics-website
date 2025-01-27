e6fe6f20e4cbcf0fc54f0d507e0df5110445e196
Yue Li <yueli@Yues-MacBook-Air.local>
2025-01-26 22:36:59 -0500

    Replace empty interfaces with type aliases

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,9 +4,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
 export const Input = React.forwardRef<
   HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
+  InputProps
 >(({ className, ...props }, ref) => {
   return (
     <input
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -4,9 +4,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
 export const Textarea = React.forwardRef<
   HTMLTextAreaElement,
-  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+  TextareaProps
 >(({ className, ...props }, ref) => {
   return (
     <textarea
